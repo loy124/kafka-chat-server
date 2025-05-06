618aa70760d7e60a8100e31f87e00be6a11f4d97
lee on you <dhsdb1541@naver.com>
2025-05-06 19:58:28 +0900

    feature: websocket 기능 추가하기
    /topic/chat/room- 로 시작하는 소켓 통신 처리하기

--- chat-server-backend/src/main/java/com/example/chat/chat_server/chat/service/ChatService.java
+++ chat-server-backend/src/main/java/com/example/chat/chat_server/chat/service/ChatService.java
@@ -2,22 +2,33 @@ package com.example.chat.chat_server.chat.service;
 
 import com.example.chat.chat_server.chat.dto.ChatMessage;
 import com.example.chat.chat_server.chat.infrastructure.kafka.ChatKafkaProducer;
+import com.example.chat.chat_server.core.websocket.WebSocketSender;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ChatService {
 
     private final ChatKafkaProducer chatKafkaProducer;
+    private final WebSocketSender webSocketSender;
+
 
     public void sendMessage(ChatMessage chatMessage) {
         chatKafkaProducer.send(chatMessage);
     }
 
     public void handleReceivedMessage(ChatMessage message) {
-        // 여기서 DB 저장, WebSocket 전송 등의 후속 작업 처리
-        System.out.println("서비스에서 수신 메시지 처리: " + message);
+        // 1. DB 저장 로직 (선택, 지금은 생략)
+
+        // 2. WebSocket 실시간 메시지 전송
+        String destination = "/topic/chat/" + "room-" + message.getRoomId();
+        webSocketSender.send(destination, message);
+
+
+        log.info("서비스에서 수신 메시지 처리: " + message);
     }
 }
 
